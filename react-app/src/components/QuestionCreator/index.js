e80e3a5ba51301e969eea99946c207f4ce251535
Larry <larrysyliao@gmail.com>
2023-01-10 17:12:30 -0800

    savepoitn

--- react-app/src/components/QuestionCreator/index.js
+++ react-app/src/components/QuestionCreator/index.js
@@ -1,55 +1,55 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function QuestionCreator() {
-  const [choices, setChoices] = useState([]);
-  const [choice, setChocie] = useState("");
-  const [question, setQuestion] = useState("");
+// export default function QuestionCreator() {
+//   const [choices, setChoices] = useState([]);
+//   const [choice, setChocie] = useState("");
+//   const [question, setQuestion] = useState("");
 
-  const handleChoiceInputChange = (e) => {
-    setChocie(e.target.value);
-  };
+//   const handleChoiceInputChange = (e) => {
+//     setChocie(e.target.value);
+//   };
 
-  const handleQuestionInputChange = (e) => {
-    setQuestion(e.target.value);
-  };
+//   const handleQuestionInputChange = (e) => {
+//     setQuestion(e.target.value);
+//   };
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-  };
+//   const handleFormSubmit = (e) => {
+//     e.preventDefault();
+//   };
 
-  const handleAddClick = (e) => {
-    console.log({ choice });
-    setChoices([...choices, choice]);
-    setChocie("");
-  };
+//   const handleAddClick = (e) => {
+//     console.log({ choice });
+//     setChoices([...choices, choice]);
+//     setChocie("");
+//   };
 
-  const handleQuestionSubmit = (e) => {
-    // should do a request to the back end
-    // post /questions
-    console.log(question, choices);
+//   const handleQuestionSubmit = (e) => {
+//     // should do a request to the back end
+//     // post /questions
+//     console.log(question, choices);
 
-    axios.post('/answers', answers)
-  };
+//     axios.post('/answers', answers)
+//   };
 
-  console.log({ choices });
+//   console.log({ choices });
 
-  return (
-    <form onSubmit={handleFormSubmit}>
-      <input placeholder="who are you?" onChange={handleQuestionInputChange} />
+//   return (
+//     <form onSubmit={handleFormSubmit}>
+//       <input placeholder="who are you?" onChange={handleQuestionInputChange} />
 
-      <div>
-        {choices.map((choice) => {
-          return <div>{choice}</div>;
-        })}
-        <input
-          value={choice}
-          placeholder="otherchoice"
-          onChange={handleChoiceInputChange}
-        />
-        <button onClick={handleAddClick}>add choice</button>
-      </div>
+//       <div>
+//         {choices.map((choice) => {
+//           return <div>{choice}</div>;
+//         })}
+//         <input
+//           value={choice}
+//           placeholder="otherchoice"
+//           onChange={handleChoiceInputChange}
+//         />
+//         <button onClick={handleAddClick}>add choice</button>
+//       </div>
 
-      <button onClick={handleQuestionSubmit}>Add Question</button>
-    </form>
-  );
-}
+//       <button onClick={handleQuestionSubmit}>Add Question</button>
+//     </form>
+//   );
+// }
